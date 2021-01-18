4940de59f37bec1ba6c904e5cd88b133d17c42b3
Marwan B <marwan.bayoumi@gmail.com>
2021-01-18 10:06:20 +0100

    add wow anime sound effect

--- cmd_handler.js
+++ cmd_handler.js
@@ -14,7 +14,6 @@ module.exports = {
         name: '!gohan',
         execute(msg, args) {
             var voiceChannel = msg.member.voice.channel;
-            // if (voiceChannel) {
                 switch (voiceChannel != null && args != null) {
                     case (args.length == 0):
                         dispatcher(voiceChannel, sfx.dbz_it)
@@ -26,16 +25,7 @@ module.exports = {
                         dispatcher(voiceChannel, sfx.oh_no)
                         break;
                 }
-            // }
         }
     }
 }
 
-/*           if (voiceChannel && args.length == 0) {
-   dispatcher(voiceChannel, sfx.dbz_it)
-            }
-            else if (voiceChannel && args.includes('p')) {
-                dispatcher(voiceChannel, sfx.dbz_bp)
-            }else{
-                msg.reply('You need to be in a voice channel.')
-            } */
