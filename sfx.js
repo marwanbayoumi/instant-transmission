d041a2d1cd372e33703251db032f943130693048
Marwan B <marwan.bayoumi@gmail.com>
2021-01-18 10:18:16 +0100

    add wow anime sound effect

--- sfx.js
+++ sfx.js
@@ -2,5 +2,6 @@ module.exports =
 {
     dbz_it: './media/dbz_it.mp3',
     dbz_bp: './media/dbz_bp.mp3',
-    oh_no: './media/oh_no.mp3'
+    oh_no: './media/oh_no.mp3',
+    anime_wow: './media/anime_wow.mp3'
 }
